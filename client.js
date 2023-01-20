536cf7848dd54e2676cb7185be54225884e82430
tabby-lab <tabby.garcia1@gmail.com>
2023-01-20 10:06:52 -0600

    adding graphql query to index.js. Created my client to capture wp graphql

---
+++ client.js
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: process.env.WP_GRAPHQL_URL,
+    cache: new InMemoryCache(),
+
+});
+
+export default client;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
+import { gql } from "@apollo/client";
+import client from 'client';
+
 export default function Home(props) {
   console.log("PROPS:", props)
-  return <div> hi </div>
+  return <div> hello </div>
 }
 
 export const getStaticProps = async () => {
+  const {data} = await client.query({
+    query:gql`
+    query NewQuery {
+      pages {
+        nodes {
+          title
+        }
+      }
+    }
+    `
+  })
   return {
     props: {
+      data,
       myexampleprops: "test"
     }
   }

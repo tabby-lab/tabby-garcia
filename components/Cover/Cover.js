25cf0d15b0956c0e9cf0b150bc17a5b8b8af33e8
tabby-lab <tabby.garcia1@gmail.com>
2023-01-23 16:33:43 -0600

    changed styles and cover.js

--- components/Cover/Cover.js
+++ components/Cover/Cover.js
@@ -18,3 +18,4 @@ export const Cover = ({ children, background }) => {
     </div>
   );
 };
+
--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
+  images: {
+    domains: [process.env.WP_IMAGES_URL],
+  },
 };
 
 module.exports = nextConfig
 
 
-// images:{
-//   domains: [process.env.WP_IMAGES_URL],
-// },
